d237644734c64b6c2a5bd1a96d6ddb0ef464523b
GalloDaniel <danielcostagallo@gmail.com>
2021-03-29 17:36:11 -0300

    Update Admins Controller

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,24 +1,47 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: %i[ update ]
+  before_action :set_admin, only: %i[ update edit ]
+
+
   def index
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador criado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
   def edit
-    @admin = Admin.find(params[:id])
   end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-    
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
     end
+  end
+
+  private
+
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
   end
 end
