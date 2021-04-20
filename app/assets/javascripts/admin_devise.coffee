3f5479130088e36f8071e1ab341db0d82578c6d0
GalloDaniel <danielcostagallo@gmail.com>
2021-04-20 12:56:46 -0300

    improving layouts from devise and site

---
+++ app/assets/javascripts/admin_devise.coffee
@@ -0,0 +1,5 @@
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap.bundle
+//= require jquery.easing.min
+//= require sb-admin-2.min
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,9 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # /app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js)
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css users_backoffice.css users_backoffice.js admin_devise.js admin_devise.css user_devise.js user_devise.css site.js site.css)
 # /lib/assets
-Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css jquery.easing.min.js all.min.css custom.css custom.js img.jpg datatables-demo.js dataTables.bootstrap4.min.js jquery.dataTables.min.js dataTables.bootstrap4.min.css )
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.js sb-admin-2.min.css jquery.easing.min.js all.min.css custom.css custom.js img.jpg datatables-demo.js dataTables.bootstrap4.min.js jquery.dataTables.min.js dataTables.bootstrap4.min.css navbar.css)
 # /vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.js bootstrapN/dist/js/bootstrap.bundle.js )
 
