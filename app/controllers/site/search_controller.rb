01fc74136cb1f4883cd620409aabedc6808f18bf
GalloDaniel <danielcostagallo@gmail.com>
2021-04-23 17:49:22 -0300

    lot of stuff

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,7 +1,9 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.includes(:answers)
-                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-                         .page(params[:page])
+    @questions = Question._search_(params[:page], params[:term])
+  end
+
+  def subject
+    @questions = Question._search_subject_(params[:page], params[:subject_id])
   end
 end
--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,13 @@
 module SiteHelper
+
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Ultimas perguntas cadastradas..."
+    when 'questions'
+      "Resuldados para o termo \"#{params[:term]}\"..."
+    when 'subject'
+      "Monstrando questões para o assunto \"#{params[:subject]}\"..."
+    end
+  end
 end
