64a67b059ec12b548bbb60778bfce61dcd43b3fc
GalloDaniel <danielcostagallo@gmail.com>
2021-04-26 12:39:25 -0300

    improving user backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :users_backoffice do
     get 'welcome/index'
     get 'profile', to: 'profile#edit'
+    patch 'profile', to: 'profile#update'
   end
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
@@ -21,6 +22,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   get 'inicio', to: 'site/welcome#index'
+  get 'admin', to: 'admins_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
